e6e1cee45ca9ce6bc0051f1b45393831cbd9a84c
AlanNois <none@iesschool.edu.vn>
2023-08-22 08:36:41 +0700

    Add Blogtruyen source

---
+++ src/Blogtruyen/Blogtruyen.ts
@@ -0,0 +1,244 @@
+import {
+    TagSection,
+    SourceManga,
+    Chapter,
+    ChapterDetails,
+    HomeSection,
+    HomeSectionType,
+    SearchRequest,
+    PagedResults,
+    Request,
+    Response,
+    ChapterProviding,
+    MangaProviding,
+    SearchResultsProviding,
+    HomePageSectionsProviding,
+    SourceInfo,
+    ContentRating,
+    SourceIntents,
+} from '@paperback/types';
+
+import { Parser } from './BlogtruyenParser';
+
+const DOMAIN = 'https://blogtruyen.vn/';
+
+export const isLastPage = ($: CheerioStatic): boolean => {
+    const lastPage = Number($('ul.pagination > li:last-child > a').attr('href')?.split('-').pop());
+    const currentPage = Number($('ul.pagination > li > select > option').find(':selected').text().split(' ')[1]);
+
+    return currentPage >= lastPage;
+}
+
+export const BlogtruyenInfo: SourceInfo = {
+    version: '1.0.0',
+    name: 'Blogtruyen',
+    icon: 'icon.png',
+    author: 'AlanNois',
+    authorWebsite: 'https://github/AlanNois',
+    description: 'Extension that pulls manga from Blogtruyen',
+    websiteBaseURL: DOMAIN,
+    contentRating: ContentRating.MATURE,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS
+}
+
+export class Blogtruyen implements SearchResultsProviding, MangaProviding, ChapterProviding, HomePageSectionsProviding {
+    constructor(private cheerio: CheerioAPI) { }
+
+    readonly requestManager = App.createRequestManager({
+        requestsPerSecond: 4,
+        requestTimeout: 15000,
+        interceptor: {
+            interceptRequest: async (request: Request): Promise<Request> => {
+                request.headers = {
+                    ...request.headers,
+                    'Referer': DOMAIN,
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+                }
+                return request;
+            },
+            interceptResponse: async (response: Response): Promise<Response> => {
+                return response;
+            }
+        }
+    });
+
+    getMangaShareUrl(mangaId: string): string {
+        return `${DOMAIN}${mangaId}`;
+    }
+
+    parser = new Parser();
+
+    private async DOMHTML(url: string): Promise<CheerioStatic> {
+        const request = App.createRequest({
+            url: url,
+            method: 'GET',
+        });
+
+        const response = await this.requestManager.schedule(request, 1);
+        return this.cheerio.load(response.data as string);
+    }
+
+    async getMangaDetails(mangaId: string): Promise<SourceManga> {
+        const $ = await this.DOMHTML(`${DOMAIN}${mangaId}`);
+        return this.parser.parseMangaDetails($, mangaId);
+    }
+
+    async getChapters(mangaId: string): Promise<Chapter[]> {
+        const $ = await this.DOMHTML(`${DOMAIN}${mangaId}`);
+        return this.parser.parseChapters($);
+    }
+
+    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+        const $ = await this.DOMHTML(`${DOMAIN}${chapterId}`);
+        const pages = this.parser.parseChapterDetails($);
+        return App.createChapterDetails({
+            id: chapterId,
+            mangaId: mangaId,
+            pages: pages,
+        })
+    }
+
+    async supportsTagExclusion(): Promise<boolean> {
+        return true;
+    }
+
+    async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
+        let page = metadata?.page ?? 1;
+
+        const params = {
+            genres: '',
+            exgenres: '',
+            status: '',
+        }
+
+        const tags = query.includedTags?.map((tag) => tag.id) ?? [];
+        const extags = query.excludedTags?.map((tag) => tag.id) ?? [];
+
+        const genres: string[] = [];
+        const exgenres: string[] = [];
+
+        for (const value of extags) {
+            if (value.indexOf('.') === -1) {
+                exgenres.push(value);
+            }
+        }
+
+        for (const value of tags) {
+            if (value.indexOf('.') === -1) {
+                genres.push(value);
+            } else {
+                const [key, val] = value.split('.');
+                switch (key) {
+                    case 'anything':
+                        params.status = String(val);
+                        break;
+                    case 'ongoing':
+                        params.status = String(val);
+                        break;
+                    case 'completed':
+                        params.status = String(val);
+                        break;
+                    case 'drop':
+                        params.status = String(val);
+                        break;
+                }
+            }
+        }
+
+        params.genres = genres.join(',');
+        params.exgenres = exgenres.join(',');
+
+        const param = encodeURI(`1/${params.status ? params.status : '0'}/${params.genres ? params.genres : '-1'}/${params.exgenres ? params.exgenres : '-1'}${query.title ? `?txt=${query.title}` : ''}${query.title ? `&p=${page}` : `?p=${page}`}`);
+        const $ = await this.DOMHTML(`${DOMAIN}timkiem/nangcao/${param}`);
+
+        const results = this.parser.parseSearchResults($);
+        metadata = isLastPage($) ? undefined : { page: page + 1 };
+
+        return App.createPagedResults({
+            results,
+            metadata
+        })
+
+    }
+
+    async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        const sections: HomeSection[] = [
+            App.createHomeSection({ id: 'featured', title: "TRUYỆN ĐỀ CỬ", containsMoreItems: false, type: HomeSectionType.featured}),
+            App.createHomeSection({ id: 'hot', title: "TRUYỆN XEM NHIỀU NHẤT", containsMoreItems: true, type: HomeSectionType.singleRowNormal}),
+            App.createHomeSection({ id: 'new_updated', title: "TRUYỆN MỚI CẬP NHẬT", containsMoreItems: true, type: HomeSectionType.singleRowNormal}),
+            App.createHomeSection({ id: 'full', title: "TRUYỆN ĐÃ HOÀN THÀNH", containsMoreItems: true, type: HomeSectionType.singleRowNormal})
+        ]
+
+        for (const section of sections) {
+            sectionCallback(section);
+            let url: string;
+            switch (section.id) {
+                case 'featured':
+                    url = DOMAIN;
+                    break;
+                case 'hot':
+                    url = `${DOMAIN}ajax/Search/AjaxLoadListManga?key=tatca&orderBy=3&p=1`;
+                    break;
+                case 'new_updated':
+                    url = `${DOMAIN}thumb`;
+                    break;
+                case 'full':
+                    url = `${DOMAIN}ajax/Category/AjaxLoadMangaByCategory?id=0&orderBy=5&p=1`;
+                    break;
+                default:
+                    throw new Error(`Invalid home section ID`);
+            }
+
+            const $ = await this.DOMHTML(url);
+            switch (section.id) {
+                case 'featured':
+                    section.items = this.parser.parseFeaturedSection($);
+                    break;
+                case 'hot':
+                    section.items = this.parser.parseAjaxSection($);
+                    break;
+                case 'new_updated':
+                    section.items = this.parser.parseNewUpdatedSection($);
+                    break;
+                case 'full':
+                    section.items = this.parser.parseAjaxSection($);
+                    break;
+            }
+            sectionCallback(section);
+        }
+    }
+
+    async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+        let page: number = metadata?.page ?? 1;
+        let url = '';
+
+        switch (homepageSectionId) {
+            case 'hot':
+                url = `${DOMAIN}ajax/Search/AjaxLoadListManga?key=tatca&orderBy=3&p=${page}`;
+                break;
+            case 'new_updated':
+                url = `${DOMAIN}thumb-${page}`;
+                break;
+            case 'full':
+                url = `${DOMAIN}ajax/Category/AjaxLoadMangaByCategory?id=0&orderBy=5&p=${page}`;
+                break;
+            default:
+                throw new Error(`Invalid home section ID`);
+        }
+
+        const $ = await this.DOMHTML(url);
+        const results = this.parser.parseViewMoreSection($, homepageSectionId);
+        metadata = isLastPage($) ? undefined : { page: page + 1 };
+
+        return App.createPagedResults({
+            results,
+            metadata
+        })
+    }
+
+    async getSearchTags(): Promise<TagSection[]> {
+        const url = `${DOMAIN}timkiem/nangcao`;
+        const $ = await this.DOMHTML(url);
+        return this.parser.parseTags($);
+    }
+}
---
+++ src/Blogtruyen/BlogtruyenParser.ts
@@ -0,0 +1,243 @@
+import {
+    Chapter,
+    SourceManga,
+    Tag,
+    TagSection,
+    PartialSourceManga
+} from '@paperback/types'
+
+const entities = require('entities');
+
+export class Parser {
+    parseMangaDetails($: CheerioStatic, mangaId: string): SourceManga {
+        const tags: Tag[] = [];
+
+        let author = '';
+        let artist = '';
+
+        $('p', '.descripton').each((_: any, obj: any) => {
+            switch ($(obj).clone().children().remove().end().text().trim()) {
+                case 'Tác giả:':
+                    author = this.decodeHTMLEntity($('a', obj).text().trim());
+                    artist = this.decodeHTMLEntity($('a', obj).text().trim());
+                    break;
+                case 'Thể loại:':
+                    $('.category > a', obj).each((_: any, genres: any) => {
+                        const genre = $(genres).text().trim();
+                        const id = $(genres).attr('href') ?? genre;
+                        tags.push(App.createTag({ label: genre, id }));
+                    });
+            }
+        })
+
+        const title = [this.decodeHTMLEntity($('.entry-title > a').text().trim())]
+        const desc = $('.content').text();
+        const image = encodeURI(String($('.thumbnail > img').attr('src'))) ?? "https://i.imgur.com/GYUxEX8.png";
+        const status = $('.description > p > .color-red:last-child').text().trim();
+
+        return App.createSourceManga({
+            id: mangaId,
+            mangaInfo: App.createMangaInfo({
+                titles: title,
+                author,
+                artist,
+                image,
+                covers: [image],
+                desc,
+                status,
+                tags: [App.createTagSection({ id: '0', label: 'genres', tags: tags })]
+            })
+        })
+    }
+
+    parseChapters($: CheerioStatic): Chapter[] {
+        const chapters: Chapter[] = [];
+
+        var chapNum = 0;
+        $('#list-chapters > p').get().reverse().forEach((obj) => {
+            const time_raw = $('.publishedDate', obj).text().trim();
+            const time = this.convert_time(time_raw);
+            const id = String($('span > a', obj).attr('href'));
+            const name = this.decodeHTMLEntity($('span > a', obj).text().trim());
+
+            chapters.push(App.createChapter({
+                id,
+                chapNum,
+                name,
+                time,
+                langCode: '🇻🇳',
+            }));
+            chapNum++;
+        });
+
+        if (chapters.length == 0) {
+            throw new Error('No chapters found');
+        }
+
+        return chapters;
+    }
+
+    parseChapterDetails($: CheerioStatic): string[] {
+        const pages: string[] = [];
+
+        $('#content > img').each((_: any, obj: any) => {
+            if (!obj.attribs['src']) return;
+            const link = obj.attribs['src'];
+            pages.push(link);
+        });
+
+        return pages;
+    }
+
+    parseSearchResults($: CheerioStatic): PartialSourceManga[] {
+        const results: PartialSourceManga[] = [];
+
+        $('p:not(:first-child)', '.list').each((_: any, obj: any) => {
+            const title = $('a', obj).text().trim();
+            const subtitle = 'Chương ' + $('span:nth-child(2)', obj).text().trim();
+            const image = $('img', $(obj).next()).attr('src') || "https://i.imgur.com/GYUxEX8.png";
+            const mangaId = String($('a', obj).attr('href'));
+            if (!mangaId || !title) return;
+
+            results.push(App.createPartialSourceManga({
+                mangaId,
+                image,
+                title,
+                subtitle
+            }))
+        });
+
+        return results;
+    }
+
+    parseFeaturedSection($: CheerioStatic): PartialSourceManga[] {
+        const featuredItems: PartialSourceManga[] = [];
+
+        $('a', '#storyPinked').each((_: any, obj: any) => {
+            const title = $('p:first-child', $(obj).next()).text().trim();
+            const mangaId = String($(obj).attr('href'));
+            const image = $('img', obj).attr('src')?.replace('300x300', '500x') || "https://i.imgur.com/GYUxEX8.png";
+            const subtitle = $('p:last-child', $(obj).next()).text().trim();
+            if (!mangaId || !title) return;
+
+            featuredItems.push(App.createPartialSourceManga({
+                mangaId,
+                image,
+                title,
+                subtitle
+            }))
+        });
+
+        return featuredItems;
+    }
+
+    parseAjaxSection($: CheerioStatic): PartialSourceManga[] {
+        const ajaxItems: PartialSourceManga[] = [];
+
+        $('p:not(:first-child)', '.list').each((_: any, obj: any) => {
+            const title = $('a', obj).text().trim();
+            const subtitle = 'Chương ' + $('span:nth-child(2)', obj).text().trim();
+            const image = $('img', $(obj).next()).attr('src') || "https://i.imgur.com/GYUxEX8.png";
+            const mangaId = String($('a', obj).attr('href'));
+            if (!mangaId || !title) return;
+            
+            ajaxItems.push(App.createPartialSourceManga({
+                mangaId,
+                image,
+                title,
+                subtitle
+            }));
+        });
+
+        return ajaxItems;
+    }
+
+    parseNewUpdatedSection($: CheerioStatic): PartialSourceManga[] {
+        const newUpdatedItems: PartialSourceManga[] = [];
+
+        $('.row', '.list-mainpage .storyitem').each((_: any, obj: any) => {
+            const title = $('h3.title > a', obj).attr('title')?.trim();
+            const subtitle = $('div:nth-child(2) > div:nth-child(4) > span:nth-child(1) > .color-red', obj).text();
+            const image = String($('div:nth-child(1) > a > img', obj).attr('src'));
+            const mangaId = $('div:nth-child(1) > a', obj).attr('href') ?? title;
+            if (!mangaId || !title) return;
+
+            newUpdatedItems.push(App.createPartialSourceManga({
+                mangaId,
+                image,
+                title,
+                subtitle
+            }));
+        });
+
+        return newUpdatedItems;
+    }
+
+    parseViewMoreSection($: CheerioStatic, homepageSectionId: any): PartialSourceManga[] {
+        switch (homepageSectionId) {
+            case 'featured':
+                return this.parseFeaturedSection($);
+            case 'hot':
+                return this.parseAjaxSection($);
+            case 'new_updated':
+                return this.parseNewUpdatedSection($);
+            default:
+                return [];
+        }
+    }
+
+    parseTags($: CheerioStatic): TagSection[] {
+        const arrayTags: Tag[] = [];
+        const arrayTags2: Tag[] = [];
+
+        // The loai
+        for (const tag of $('li', '.list-unstyled.row').toArray()) {
+            const label = this.decodeHTMLEntity($(tag).text().trim());
+            const id = $(tag).attr('data-id') ?? label;
+            if (!id || !label) continue;
+            arrayTags.push({ id: id, label: label });
+        }
+
+        // Tinh trang
+        for (const tag of $('option', '#Status').toArray()) {
+            const label = this.decodeHTMLEntity($(tag).text().trim());
+            let id : string;
+            switch (label) {
+                case 'Sao cũng được':
+                    id = 'anything' + $(tag).attr('value') ?? label;
+                    break;
+                case 'Đang tiến hành':
+                    id = 'ongoing' + $(tag).attr('value') ?? label;
+                    break;
+                case 'Đã hoàn thành':
+                    id = 'completed' + $(tag).attr('value') ?? label;
+                    break;
+                case 'Tạm ngưng':
+                    id = 'drop' + $(tag).attr('value') ?? label;
+                    break;
+                default:
+                    id = $(tag).attr('value') ?? label;
+            }
+            if (!id || !label) continue;
+            arrayTags2.push({ id: id, label: label });
+        }
+
+        const tagSections: TagSection[] = [
+            App.createTagSection({ id: '0', label: 'Thể loại (Chọn nhiều)', tags: arrayTags.map(x => App.createTag(x))}),
+            App.createTagSection({ id: '1', label: 'Tình trạng (Chọn 1)', tags: arrayTags2.map(x => App.createTag(x))}),
+        ];
+
+        return tagSections;
+    }
+
+    private decodeHTMLEntity = (str: string): string => {
+        return entities.decodeHTML(str);
+    };
+
+    private convert_time = (timeAgo: string): Date => {
+        const [D, H] = timeAgo.split(' ');
+        const fixD = String(D).split('/');
+        const finalD = `${fixD[1]}/${fixD[0]}/${fixD[2]}`;
+        return new Date(`${finalD} ${H}`)
+    }
+}
