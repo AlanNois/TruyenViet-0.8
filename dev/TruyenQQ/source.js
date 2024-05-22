7a1de8ae64f21f69317a5b7a0f458d6a4bc73011
AlanNois <none@iesschool.edu.vn>
2024-05-22 22:10:50 +0700

    Updates

--- dev/TruyenQQ/source.js
+++ dev/TruyenQQ/source.js
@@ -593,7 +593,7 @@ class TruyenQQ {
         search.genres = genres.join(",");
         search.exgenres = exgenres.join(",");
         const paramExgenres = search.exgenres ? `&notgenres=${search.exgenres}` : '';
-        const url = `${DOMAIN}${query.title ? '/tim-kiem' : '/tim-kiem-nang-cao'}/trang-${page}.html`;
+        const url = `${DOMAIN}${query.title ? 'tim-kiem' : 'tim-kiem-nang-cao'}/trang-${page}.html`;
         const param = encodeURI(`?q=${query.title ?? ''}&genres=${search.genres}${paramExgenres}&country=${search.country}&status=${search.status}&minchapter=${search.minchapter}&sort=${search.sort}`);
         const $ = await this.DOMHTML(url + param);
         const tiles = this.parser.parseSearchResults($);
