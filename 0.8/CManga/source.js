0fda6aa20941daccd2014fd93ac6d5b2162bf0ad
AlanNois <none@iesschool.edu.vn>
2025-04-21 22:16:16 +0700

    Updates

--- 0.8/CManga/source.js
+++ 0.8/CManga/source.js
@@ -1438,9 +1438,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CManga = exports.CMangaInfo = void 0;
 const types_1 = require("@paperback/types");
 const CMangaParser_1 = require("./CMangaParser");
-const DOMAIN = 'https://cmangaax.com/';
+const DOMAIN = 'https://cmangax1.com/';
 exports.CMangaInfo = {
-    version: '1.0.16',
+    version: '1.0.17',
     name: 'CManga',
     icon: 'icon.png',
     author: 'AlanNois',
