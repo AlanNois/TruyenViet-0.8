27a9416d6cb03fc077f09e15d83969115786027d
AlanNois <sealonghoang@gmail.com>
2025-08-28 10:47:32 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@b35e82553be7ad4a15bc766d6f708e5efa8d5c19 🚀

--- dev/DocTruyen3Q/index.js
+++ dev/DocTruyen3Q/index.js
@@ -471,7 +471,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.DocTruyen3QInfo = {
-    version: '1.1.15',
+    version: '1.1.16',
     name: 'DocTruyen3Q',
     icon: 'icon.png',
     author: 'AlanNois',
@@ -563,7 +563,7 @@ class DocTruyen3Q {
                     break;
             }
         }
-        const url = `${DOMAIN}/tim-truyen/`;
+        const url = `${DOMAIN}tim-truyen`;
         const search_query = (!query.title) ? '?' : `?keyword=${query.title}&`;
         const param = `${search.cate}${search_query}sort=${search.sort}&status=${search.status}&page=${page}`;
         const $ = await this.DOMHTML(`${url}${encodeURI(param)}`);
