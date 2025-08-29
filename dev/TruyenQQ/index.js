069b75bc836ff7b13faaff94bff4bade697811b3
AlanNois <sealonghoang@gmail.com>
2025-08-29 10:24:01 +0700

    Updates

--- dev/TruyenQQ/index.js
+++ dev/TruyenQQ/index.js
@@ -477,7 +477,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.TruyenQQInfo = {
-    version: '1.0.9',
+    version: '1.0.10',
     name: 'TruyenQQ',
     icon: 'icon.png',
     author: 'AlanNois',
@@ -551,7 +551,7 @@ class TruyenQQ {
         return true;
     }
     async getSearchResults(query, metadata) {
-        let page = metadata?.page ?? 1;
+        const page = metadata?.page ?? 1;
         const search = {
             genres: '',
             exgenres: '',
@@ -574,7 +574,7 @@ class TruyenQQ {
                 genres.push(value);
             }
             else {
-                const [key, val] = value.split(".");
+                const [key, val] = value.split('.');
                 switch (key) {
                     case 'minchapter':
                         search.minchapter = String(val);
@@ -591,8 +591,8 @@ class TruyenQQ {
                 }
             }
         }
-        search.genres = genres.join(",");
-        search.exgenres = exgenres.join(",");
+        search.genres = genres.join(',');
+        search.exgenres = exgenres.join(',');
         const paramExgenres = search.exgenres ? `&notcategory==${search.exgenres}` : '';
         const url = `${DOMAIN}${query.title ? 'tim-kiem' : 'tim-kiem-nang-cao'}/trang-${page}.html`;
         const param = encodeURI(`?q=${query.title ?? ''}&category=${search.genres}${paramExgenres}&country=${search.country}&status=${search.status}&minchapter=${search.minchapter}&sort=${search.sort}`);
@@ -607,11 +607,11 @@ class TruyenQQ {
     async getHomePageSections(sectionCallback) {
         console.log('TruyenQQ Running...');
         const sections = [
-            App.createHomeSection({ id: 'featured', title: "Truyện Đề Cử", containsMoreItems: false, type: types_1.HomeSectionType.featured }),
-            App.createHomeSection({ id: 'hot', title: "Truyện Yêu Thích", containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
-            App.createHomeSection({ id: 'new_updated', title: "Truyện Mới Cập Nhật", containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
-            App.createHomeSection({ id: 'new_added', title: "Truyện Mới Thêm Gần Đây", containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
-            App.createHomeSection({ id: 'full', title: "Truyện Đã Hoàn Thành", containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
+            App.createHomeSection({ id: 'featured', title: 'Truyện Đề Cử', containsMoreItems: false, type: types_1.HomeSectionType.featured }),
+            App.createHomeSection({ id: 'hot', title: 'Truyện Yêu Thích', containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
+            App.createHomeSection({ id: 'new_updated', title: 'Truyện Mới Cập Nhật', containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
+            App.createHomeSection({ id: 'new_added', title: 'Truyện Mới Thêm Gần Đây', containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
+            App.createHomeSection({ id: 'full', title: 'Truyện Đã Hoàn Thành', containsMoreItems: true, type: types_1.HomeSectionType.singleRowNormal }),
         ];
         for (const section of sections) {
             sectionCallback(section);
@@ -633,7 +633,7 @@ class TruyenQQ {
                     url = `${DOMAIN}truyen-hoan-thanh.html`;
                     break;
                 default:
-                    throw new Error("Invalid homepage section ID");
+                    throw new Error('Invalid homepage section ID');
             }
             const $ = await this.DOMHTML(url);
             switch (section.id) {
@@ -657,28 +657,28 @@ class TruyenQQ {
         }
     }
     async getViewMoreItems(homepageSectionId, metadata) {
-        let page = metadata?.page ?? 1;
+        const page = metadata?.page ?? 1;
         let param = "";
         let url = "";
         switch (homepageSectionId) {
-            case "hot":
+            case 'hot':
                 param = `trang-${page}.html`;
                 url = `${DOMAIN}truyen-yeu-thich/`;
                 break;
-            case "new_updated":
+            case 'new_updated':
                 param = `trang-${page}.html`;
                 url = `${DOMAIN}truyen-moi-cap-nhat/`;
                 break;
-            case "new_added":
+            case 'new_added':
                 param = `trang-${page}.html`;
                 url = `${DOMAIN}truyen-tranh-moi/`;
                 break;
-            case "full":
+            case 'full':
                 param = `trang-${page}.html?status=2`;
                 url = `${DOMAIN}truyen-hoan-thanh/`;
                 break;
             default:
-                throw new Error("Requested to getViewMoreItems for a section ID which doesn't exist");
+                throw new Error('Requested to getViewMoreItems for a section ID which doesn\'t exist');
         }
         const request = App.createRequest({
             url,
