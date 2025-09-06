f5bc027b0794c8d1c0c81a9e7d43d08462066f78
AlanNois <sealonghoang@gmail.com>
2025-09-06 21:18:38 +0700

    Fixes image loading for TopTruyen
    Updates the image attribute used for parsing from `data-original` to `src` to adapt to recent website changes. This ensures manga images load correctly.
    
    Bumps the source version.

--- src/TopTruyen/TopTruyen.ts
+++ src/TopTruyen/TopTruyen.ts
@@ -40,7 +40,7 @@ export const isLastPage = ($: CheerioStatic): boolean => {
 };
 
 export const TopTruyenInfo: SourceInfo = {
-    version: '1.0.1',
+    version: '1.0.2',
     name: 'TopTruyen',
     icon: 'icon.png',
     author: 'AlanNois',
--- src/TopTruyen/TopTruyenParser.ts
+++ src/TopTruyen/TopTruyenParser.ts
@@ -197,7 +197,7 @@ export class Parser {
         $('div.item', 'div.row').each((_: any, manga: any) => {
             const title = $('.clearfix > .caption > h3 > a', manga).first().text();
             const id = $('.clearfix > div.image-item > a', manga).attr('href')?.split('/truyen-tranh/').pop();
-            const image = $('.clearfix > div.image-item > a > img', manga).first().attr('data-original');
+            const image = $('.clearfix > div.image-item > a > img', manga).first().attr('src');
             const subtitle = $('.clearfix > .caption > ul > li.chapter-detail:nth-of-type(1) > a', manga).last().text().trim();
             if (!id || !title) return;
             sectionItems.push(App.createPartialSourceManga({
