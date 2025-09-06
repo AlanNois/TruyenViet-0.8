3914427ae730000c0e31d99ae420a97fb5ac1572
AlanNois <sealonghoang@gmail.com>
2025-09-06 14:19:11 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@f5bc027b0794c8d1c0c81a9e7d43d08462066f78 🚀

--- dev/TopTruyen/source.js
+++ dev/TopTruyen/source.js
@@ -478,7 +478,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.TopTruyenInfo = {
-    version: '1.0.1',
+    version: '1.0.2',
     name: 'TopTruyen',
     icon: 'icon.png',
     author: 'AlanNois',
@@ -901,7 +901,7 @@ class Parser {
         $('div.item', 'div.row').each((_, manga) => {
             const title = $('.clearfix > .caption > h3 > a', manga).first().text();
             const id = $('.clearfix > div.image-item > a', manga).attr('href')?.split('/truyen-tranh/').pop();
-            const image = $('.clearfix > div.image-item > a > img', manga).first().attr('data-original');
+            const image = $('.clearfix > div.image-item > a > img', manga).first().attr('src');
             const subtitle = $('.clearfix > .caption > ul > li.chapter-detail:nth-of-type(1) > a', manga).last().text().trim();
             if (!id || !title)
                 return;
