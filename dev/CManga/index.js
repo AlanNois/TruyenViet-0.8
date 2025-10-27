61ac72545403599f623d128d0d2aa7d9d9c3be54
AlanNois <sealonghoang@gmail.com>
2025-10-27 15:44:07 +0700

    Updates

--- dev/CManga/index.js
+++ dev/CManga/index.js
@@ -1438,9 +1438,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CManga = exports.CMangaInfo = void 0;
 const types_1 = require("@paperback/types");
 const CMangaParser_1 = require("./CMangaParser");
-const DOMAIN = 'https://cmangax6.com/';
+const DOMAIN = 'https://cmangax7.com/';
 exports.CMangaInfo = {
-    version: '1.0.21',
+    version: '1.0.22',
     name: 'CManga',
     icon: 'icon.png',
     author: 'AlanNois',
