78023eadeeb58e7bf1b8611d8cc28e4e69497fc7
AlanNois <sealonghoang@gmail.com>
2026-02-14 02:50:46 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@042d67dfbe99845ac89f67e85c8eb18670f3c1ca 🚀

--- dev/CManga/source.js
+++ dev/CManga/source.js
@@ -1438,9 +1438,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CManga = exports.CMangaInfo = void 0;
 const types_1 = require("@paperback/types");
 const CMangaParser_1 = require("./CMangaParser");
-const DOMAIN = 'https://cmangax12.com/';
+const DOMAIN = 'https://cmangax13.com/';
 exports.CMangaInfo = {
-    version: '1.1.0',
+    version: '1.1.1',
     name: 'CManga',
     icon: 'icon.png',
     author: 'AlanNois',
