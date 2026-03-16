519ee9f53b683e14bff8781e7436b230f4b01809
AlanNois <sealonghoang@gmail.com>
2026-03-16 08:37:21 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@740df8996cec591cd44f067bfa8206ecfeeb51cb 🚀

--- dev/TopTruyen/index.js
+++ dev/TopTruyen/index.js
@@ -463,7 +463,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TopTruyen = exports.TopTruyenInfo = exports.isLastPage = void 0;
 const types_1 = require("@paperback/types");
 const TopTruyenParser_1 = require("./TopTruyenParser");
-const DOMAIN = 'https://www.toptruyenxz.com/';
+const DOMAIN = 'https://www.toptruyenssz.com/';
 const isLastPage = ($) => {
     // try with span else a tag
     let current = $('ul.pagination li.page-item.active span').text();
@@ -481,7 +481,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.TopTruyenInfo = {
-    version: '1.1.2',
+    version: '1.1.3',
     name: 'TopTruyen',
     icon: 'icon.png',
     author: 'AlanNois',
