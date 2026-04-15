e409bfa18bf0f2b63e88b1a46e52c637b0202b08
AlanNois <sealonghoang@gmail.com>
2026-04-15 10:47:11 +0700

    fix(GocTruyenTranh): Update domain URL and increment version to '1.2.6'

--- src/GocTruyenTranh/GocTruyenTranh.ts
+++ src/GocTruyenTranh/GocTruyenTranh.ts
@@ -21,11 +21,11 @@ import {
 
 import { Parser } from './GocTruyenTranhParser';
 
-const DOMAIN = 'https://goctruyentranhvui21.com/';
+const DOMAIN = 'https://goctruyentranhvui22.com/';
 const Auth = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqbmkgcHJhdHR2b25kYSIsImNvbWljSWRzIjpbXSwicm9sZUlkIjpudWxsLCJncm91cElkIjpudWxsLCJhZG1pbiI6ZmFsc2UsInJhbmsiOjAsInBlcm1pc3Npb24iOltdLCJpZCI6IjAwMDExNjg0MzkiLCJ0ZWFtIjpmYWxzZSwiaWF0IjoxNzY3ODAzNDc4LCJlbWFpbCI6Im51bGwifQ.eWFypaV4dDZ_R5J9Gf0HqkbLaQDWCVwuja4yJJafl6KmPgaRk9TRHHX-0X94rP6xQtpeZRS25RNjOT0RpIdffg';
 
 export const GocTruyenTranhInfo: SourceInfo = {
-    version: '1.2.5',
+    version: '1.2.6',
     name: 'GocTruyenTranh',
     icon: 'icon.png',
     author: 'AlanNois',
