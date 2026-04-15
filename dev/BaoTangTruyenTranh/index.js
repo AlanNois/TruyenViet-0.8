d14fcd6f5d0185605dea86d9905c6a9d4c9ee507
AlanNois <sealonghoang@gmail.com>
2026-04-15 03:41:56 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@956f8ae86bb6cceff9886a4861ad4aeef248abd3 🚀

--- dev/BaoTangTruyenTranh/index.js
+++ dev/BaoTangTruyenTranh/index.js
@@ -1438,7 +1438,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BaoTangTruyenTranh = exports.BaoTangTruyenTranhInfo = exports.isLastPage = void 0;
 const types_1 = require("@paperback/types");
 const BaoTangTruyenTranhParser_1 = require("./BaoTangTruyenTranhParser");
-const DOMAIN = 'https://baotangtruyen36.top/';
+const DOMAIN = 'https://baotangtruyen37.top/';
 const API = 'https://api.chilltruyentranh.site/';
 // export const isLastPage = ($: CheerioStatic): boolean => {
 //     const pages: number[] = [];
@@ -1460,7 +1460,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.BaoTangTruyenTranhInfo = {
-    version: '1.1.12',
+    version: '1.1.13',
     name: 'BaoTangTruyenTranh',
     icon: 'icon.png',
     author: 'AlanNois',
