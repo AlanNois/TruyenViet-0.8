df2264fcc0cab7eb459059dd260b3e4a808e7000
AlanNois <sealonghoang@gmail.com>
2026-05-09 15:02:16 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@24625bf347a317e9d6b0c62fc62223acdc8fd4dd 🚀

--- dev/DocTruyen3Q/source.js
+++ dev/DocTruyen3Q/source.js
@@ -471,7 +471,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.DocTruyen3QInfo = {
-    version: '1.2.6',
+    version: '1.2.7',
     name: 'DocTruyen3Q',
     icon: 'icon.png',
     author: 'AlanNois',
