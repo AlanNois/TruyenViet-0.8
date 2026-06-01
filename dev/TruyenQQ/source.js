131f429e516c5fc2379683e604cad915799b5f4c
AlanNois <sealonghoang@gmail.com>
2026-06-01 12:21:18 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@270f38466c7577f063916d1b8e07a3e96360c118 🚀

--- dev/TruyenQQ/source.js
+++ dev/TruyenQQ/source.js
@@ -594,11 +594,10 @@ class TruyenQQ {
         search.genres = genres.join(',');
         search.exgenres = exgenres.join(',');
         const paramExgenres = search.exgenres ? `&notcategory==${search.exgenres}` : '';
-        const url = `${DOMAIN}${query.title ? 'tim-kiem' : 'tim-kiem-nang-cao'}/trang-${page}.html`;
-        const param = encodeURI(`?q=${query.title ?? ''}
-            &category=${search.genres}${paramExgenres}
-            &country=${search.country}&status=${search.status}
-            &minchapter=${search.minchapter}&sort=${search.sort}`);
+        const url = `${DOMAIN}${query.title ? 'tim-kiem' : 'tim-kiem-nang-cao'}/trang-${page}`;
+        const param = `?q=${query.title?.replaceAll(" ", "%20") ?? ''}` + encodeURI(`&category=${search.genres}${paramExgenres}&country=${search.country}&status=${search.status}&minchapter=${search.minchapter}&sort=${search.sort}`);
+        ;
+        console.log('Search URL:', url + param);
         const $ = await this.DOMHTML(url + param);
         const tiles = this.parser.parseSearchResults($);
         metadata = !(0, exports.isLastPage)($) ? { page: page + 1 } : undefined;
@@ -624,16 +623,16 @@ class TruyenQQ {
                     url = `${DOMAIN}`;
                     break;
                 case 'hot':
-                    url = `${DOMAIN}truyen-yeu-thich.html`;
+                    url = `${DOMAIN}truyen-yeu-thich`;
                     break;
                 case 'new_updated':
-                    url = `${DOMAIN}truyen-moi-cap-nhat.html`;
+                    url = `${DOMAIN}truyen-moi-cap-nhat`;
                     break;
                 case 'new_added':
-                    url = `${DOMAIN}truyen-tranh-moi.html`;
+                    url = `${DOMAIN}truyen-tranh-moi`;
                     break;
                 case 'full':
-                    url = `${DOMAIN}truyen-hoan-thanh.html`;
+                    url = `${DOMAIN}truyen-hoan-thanh`;
                     break;
                 default:
                     throw new Error('Invalid homepage section ID');
@@ -665,19 +664,19 @@ class TruyenQQ {
         let url = '';
         switch (homepageSectionId) {
             case 'hot':
-                param = `trang-${page}.html`;
+                param = `trang-${page}`;
                 url = `${DOMAIN}truyen-yeu-thich/`;
                 break;
             case 'new_updated':
-                param = `trang-${page}.html`;
+                param = `trang-${page}`;
                 url = `${DOMAIN}truyen-moi-cap-nhat/`;
                 break;
             case 'new_added':
-                param = `trang-${page}.html`;
+                param = `trang-${page}`;
                 url = `${DOMAIN}truyen-tranh-moi/`;
                 break;
             case 'full':
-                param = `trang-${page}.html?status=2`;
+                param = `trang-${page}?status=2`;
                 url = `${DOMAIN}truyen-hoan-thanh/`;
                 break;
             default:
