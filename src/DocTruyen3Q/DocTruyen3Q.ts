2df7525e2f5ed8e535d373157afe473021ee4738
AlanNois <sealonghoang@gmail.com>
2026-06-13 18:58:42 +0700

    fix(DocTruyen3Q): Update domain URL and increment version to 1.2.8

--- src/DocTruyen3Q/DocTruyen3Q.ts
+++ src/DocTruyen3Q/DocTruyen3Q.ts
@@ -23,7 +23,7 @@ import {
 import type { CheerioAPI } from 'cheerio';
 import { Parser } from './DocTruyen3QParser';
 
-const DOMAIN = 'https://doctruyen3qhubz.net/';
+const DOMAIN = 'https://doctruyen3qhub.live/';
 
 export const isLastPage = ($: CheerioAPI): boolean => {
     const lastPage = Number($('ul.pagination > li.page-item:not(:has(a[rel=\'next\'])) a').last().text().trim());
@@ -33,7 +33,7 @@ export const isLastPage = ($: CheerioAPI): boolean => {
 };
 
 export const DocTruyen3QInfo: SourceInfo = {
-    version: '1.2.7',
+    version: '1.2.8',
     name: 'DocTruyen3Q',
     icon: 'icon.png',
     author: 'AlanNois',
