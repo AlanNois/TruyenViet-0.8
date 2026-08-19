f01dcc4b76a9415eb3764ab346b909a9015d1049
dinhtrung0706 <dinhductrung0706@gmail.com>
2026-08-19 08:43:45 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@1a0f07b827ddd33863c2cbd150ae6ab96f8c3de6 🚀

--- dev/GocTruyenTranh/index.js
+++ dev/GocTruyenTranh/index.js
@@ -1438,10 +1438,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.GocTruyenTranh = exports.GocTruyenTranhInfo = void 0;
 const types_1 = require("@paperback/types");
 const GocTruyenTranhParser_1 = require("./GocTruyenTranhParser");
-const DOMAIN = 'https://goctruyentranhvui30.com/';
+const DOMAIN = 'https://goctruyentranhvui41.com/';
 const Auth = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqbmkgcHJhdHR2b25kYSIsImNvbWljSWRzIjpbXSwicm9sZUlkIjpudWxsLCJncm91cElkIjpudWxsLCJhZG1pbiI6ZmFsc2UsInJhbmsiOjAsInBlcm1pc3Npb24iOltdLCJpZCI6IjAwMDExNjg0MzkiLCJ0ZWFtIjpmYWxzZSwiaWF0IjoxNzY3ODAzNDc4LCJlbWFpbCI6Im51bGwifQ.eWFypaV4dDZ_R5J9Gf0HqkbLaQDWCVwuja4yJJafl6KmPgaRk9TRHHX-0X94rP6xQtpeZRS25RNjOT0RpIdffg';
 exports.GocTruyenTranhInfo = {
-    version: '1.2.9',
+    version: '1.2.10',
     name: 'GocTruyenTranh',
     icon: 'icon.png',
     author: 'AlanNois',
