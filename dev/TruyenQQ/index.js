e6eb8035f33e448db136f9ced268fdb6c6a89e58
dinhtrung0706 <dinhductrung0706@gmail.com>
2026-08-19 08:41:01 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@1830f72cfd9eacb466e1179556a363dc311c081c 🚀

--- dev/TruyenQQ/index.js
+++ dev/TruyenQQ/index.js
@@ -477,7 +477,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.TruyenQQInfo = {
-    version: '1.1.4',
+    version: '1.1.5',
     name: 'TruyenQQ',
     icon: 'icon.png',
     author: 'AlanNois',
@@ -619,7 +619,7 @@ class TruyenQQ {
             let url;
             switch (section.id) {
                 case 'featured':
-                    url = `${DOMAIN}`;
+                    url = `${DOMAIN}doc-truyen`;
                     break;
                 case 'hot':
                     url = `${DOMAIN}truyen-yeu-thich`;
