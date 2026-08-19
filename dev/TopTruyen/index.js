c609d7582f0c3fbcaa46ac5b34ec75917b5328a4
dinhtrung0706 <dinhductrung0706@gmail.com>
2026-08-19 09:11:25 +0000

    Deploying to gh-pages from @ AlanNois/TruyenViet-0.8@805c459c03c2b7af6b0fe1a2567b618e6a8a8e18 🚀

--- dev/TopTruyen/index.js
+++ dev/TopTruyen/index.js
@@ -463,7 +463,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TopTruyen = exports.TopTruyenInfo = exports.isLastPage = void 0;
 const types_1 = require("@paperback/types");
 const TopTruyenParser_1 = require("./TopTruyenParser");
-const DOMAIN = 'https://www.toptruyenzone3.com/';
+const DOMAIN = 'https://www.toptruyenzone10.com/';
 const isLastPage = ($) => {
     // try with span else a tag
     let current = $('ul.pagination li.page-item.active span').text();
@@ -481,7 +481,7 @@ const isLastPage = ($) => {
 };
 exports.isLastPage = isLastPage;
 exports.TopTruyenInfo = {
-    version: '1.1.5',
+    version: '1.1.6',
     name: 'TopTruyen',
     icon: 'icon.png',
     author: 'AlanNois',
